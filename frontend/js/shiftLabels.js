27e27a674d4b1fc17b5c2aa614dd5ec240ae1722
Juan Pablo Serrano <jpserranogamarra@gmail.com>
2024-10-31 10:41:55 -0300

    shiftScheduledIndividualized

---
+++ frontend/js/shiftLabels.js
@@ -0,0 +1,15 @@
+// shiftAssignmentLabels.js
+
+export const shiftAssignmentLabels = {
+    Fn: "Fundación",
+    Im: "Imágenes",
+    Cp: "Hospital Cipolletti",
+    Al: "Hospital Allen",
+    Jb: "Hospital Jacobacci",
+    Ce: "Hospital Centenario",
+    HH: "Hospital Heller",
+    CM: "Hospital Chos Malal",
+    P1: "Planta Sábado",
+
+    // Agrega más equivalencias aquí según sea necesario
+};
