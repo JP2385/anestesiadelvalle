ec7b032ead30aeed4969d49f5180310e0c65c532
JP2385 <jpserranogamarra@gmail.com>
2026-01-26 00:46:27 -0300

    seguridad

--- frontend/js/openfile.js
+++ frontend/js/openfile.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     window.openFile = function(url) {
-        window.open(url, '_blank');
+        var newWin = window.open(url, '_blank');
+        try {
+            if (newWin) newWin.opener = null;
+        } catch (e) {
+            // ignore
+        }
     }
 });
